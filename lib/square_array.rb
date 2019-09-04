19199ffa457249a5b958638a47a0e74b5e80ba2f
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-04 20:45:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,13 @@
 def square_array(array)
   counter = 0 
+  new_array = []
   
   while counter < array.length do
-    array[counter] ** 2
+    squared = array[counter] ** 2
+     puts squared
+    new_array << squared
     
     counter += 1
   end
+  new_array
 end
